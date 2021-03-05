efa85206816e157615007585d7dfe158c3a5b319
Abdirahman <abdiyuu7@gmail.com>
2021-03-04 23:08:23 -0700

    refactored entire project

--- covidalbertabot.py
+++ covidalbertabot.py
@@ -1,261 +1,113 @@
-#import required libraries + modules and aliases
-import datetime
-import pytz
-import requests
-import os
-from os import environ
-import sys
-from bs4 import BeautifulSoup
-import pandas as pd
-import emoji
-import tweepy
+# %%
+from modules import *
+
+url = 'https://www.alberta.ca/covid-19-alberta-data.aspx'
+r = requests.get(url)
+r_html = r.text
+soup = BeautifulSoup(r_html, 'html.parser') 
 
 
-#grab the current date and time, format it for tweeting and data entry 
+#new df - specific locations, cases, and hospitalizations - locations as index
+table = soup.find('table')
+dataframe = pd.read_html(str(table))[0]
+dataframe = dataframe.iloc[[1, 2, 3, 4, 5, 6]]
+data = dataframe[['Location', 'Active  cases*', 'In  hospital**', 'In intensive  care***']].copy()
+data = data.set_index('Location')
 time = datetime.datetime.now().strftime("%B %d")
-timeStatus = datetime.datetime.now().strftime("%B %d, %Y")
-timenoUpdate = datetime.datetime.now(pytz.utc)
 
-def browse():
-    URL = 'https://www.alberta.ca/covid-19-alberta-data.aspx'
-    abHealth = requests.get(URL).text #use requests to get the content of the server's response (ie. the webpage)
-    return abHealth #return the page content
+#when was the table last updated
+table_comments = soup.find('em')
+text = table_comments.get_text()
+lines = text.split('*')
+latest_update = lines[0]
 
-abHealth = browse()
+#create region object class to easily identify and obtain regional data
+class Region:
+    def __init__(self, location, data_table):
+        self.location = location
+        self.data = data_table
 
-def parse():
-    #pass the webpage contents to BeautifulSoup for parsing
-    soup = BeautifulSoup(abHealth, 'html.parser')
-    #identify the table via it's div's ID
-    caseTable = soup.find('div', id="goa-grid28054")
+    def cases(self):
+        #the ab health website has two spaces between 'Active' and 'Cases' on their column name
+        return self.data.loc[self.location]['Active  cases*']
 
-    #identify the extra details and comments beneath the table encased in the <em> tag
-    global tableInfo
-    tableInfo = soup.find('em')
-
-    return caseTable
-
-caseTable = parse()
-
-
-tableInfo  = str(tableInfo)
-
-#removes the <br/> and <em> tags to recieve the first line of update info beneath the table
-lastUpdated = tableInfo.split("<br/>")[0]
-lastUpdated = lastUpdated.split("<em>", 1)[-1]
-
-def dataframe():
-    #turns the table into a dataframe using pandas + speciFies which table in the div it is via it's index (even if there's only one)
-    caseInfo = pd.read_html(str(caseTable))[0]
-    return caseInfo
-
-caseInfo = dataframe()
-
-
-#drop every location except alberta
-#drop every column except Active Cases, Hospitalizations, and Deaths
-def simplifyWithDate(df, date):
-    infoWithDate = caseInfo.copy()
-    infoWithDate.drop([0,2,3,4,5,6,7], axis=0, inplace=True)
-    infoWithDate.drop(infoWithDate.columns[[0,1,3,5,7,8]], axis=1, inplace=True)
-    infoWithDate.insert(0,'Date', [time], True)
-
-    return infoWithDate
-
-infoWithDate = simplifyWithDate(caseInfo, time)
-
-#function to add data to the csv file
-def addData(df, SQLPATH):
-    print("placeholder function addData")
-
-
-"""with open (environ.get('SQLPATH'), 'a') as f:
-        #if it exists and is empty, add data
-        if os.path.isfile(environ.get('SQLPATH')) and os.path.getsize(environ.get('SQLPATH')) == 0:
-
-        #if it exists and is full, grab the last four rows of the last column, call addData
-        elif os.path.isfile(environ.get('SQLPATH')) and os.path.getsize(environ.get('SQLPATH')) > 0:
-            addData(infoWithDate, environ.get('SQLPATH'))
-
-        #if it doesnt exist, create it and add data
-        elif not os.path.isfile(environ.get('SQLPATH')):
-
-        else:
-            print("Something's wrong... Check your code")
-            sys.exit()
-"""
-
-def locations():
-    #store the names of each location
-    alberta = caseInfo.values[1,0]
-    calgary = caseInfo.values[2,0]
-    edmonton = caseInfo.values[3,0]
-    central = caseInfo.values[4,0]
-    south = caseInfo.values[5,0]
-    north = caseInfo.values[6,0]
-    
-    return alberta, calgary, edmonton, central, south, north
-
-alberta, calgary, edmonton, central, south, north = locations()
-
-
-
-def cases():
-    #stores the values of total and active cases by location
-    albertaTotal = caseInfo.values[1,1]
-    albertaActive = caseInfo.values[1,2]
-    calgaryTotal = caseInfo.values[2,1]
-    calgaryActive = caseInfo.values[2,2]
-    edmontonTotal = caseInfo.values[3,1]
-    edmontonActive = caseInfo.values[3,2]
-    centralTotal = caseInfo.values[4,1]
-    centralActive = caseInfo.values[4,2]
-    southTotal = caseInfo.values[5,1]
-    southActive = caseInfo.values[5,2]
-    northTotal = caseInfo.values[6,1]
-    northActive = caseInfo.values[6,2]
-    
-    return albertaTotal, albertaActive, calgaryTotal, calgaryActive, edmontonTotal, edmontonActive, centralTotal, centralActive, southTotal, southActive, northTotal, northActive
-
-albertaTotal, albertaActive, calgaryTotal, calgaryActive, edmontonTotal, edmontonActive, centralTotal, centralActive, southTotal, southActive, northTotal, northActive = cases()
-
-def hospitalizations():
-    #stores the values of current hospitalizations by location
-    albertaHospital = caseInfo.values[1,4]
-    calgaryHospital = caseInfo.values[2,4]
-    edmontonHospital = caseInfo.values[3,4]
-    centralHospital = caseInfo.values[4,4]
-    southHospital = caseInfo.values[5,4]
-    northHospital = caseInfo.values[6,4]
-
-    return albertaHospital, calgaryHospital, edmontonHospital, centralHospital, southHospital, northHospital
-
-albertaHospital, calgaryHospital, edmontonHospital, centralHospital, southHospital, northHospital = hospitalizations()
-
-
-def deaths():
-    #stores the values of total deaths by location
-    albertaDeaths = caseInfo.values[1,6]
-    calgaryDeaths = caseInfo.values[2,6]
-    edmontonDeaths = caseInfo.values[3,6]
-    centralDeaths = caseInfo.values[4,6]
-    southDeaths = caseInfo.values[5,6]
-    northDeaths = caseInfo.values[6,6]
-
-    return albertaDeaths, calgaryDeaths, edmontonDeaths, centralDeaths, southDeaths, northDeaths
-
-albertaDeaths, calgaryDeaths, edmontonDeaths, centralDeaths, southDeaths, northDeaths = deaths()
-
-
-
-def authenticateTwitter():
-    #Authenticate to Twitter using the API keys you obtained from your developer account - stored in environment variables
-    auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"), 
-    os.environ.get("CONSUMER_SECRET"))
-    auth.set_access_token(os.environ.get("ACCESS_TOKEN"), 
-    os.environ.get("ACCESS_TOKEN_SECRET"))
-
-    #create an authenticated object to interact with twitter's API
-    global covidAlberta
-    covidAlberta = tweepy.API(auth)
-
-
-
-#tweet the update results
-def tweet():
-
-    try:
-        caseUpdate = covidAlberta.update_status(emoji.emojize(
-""":medical_symbol: Alberta Covid-19 Update - %s
-
-Active Cases:
+    def hospital(self):
+        return self.data.loc[self.location]['In  hospital**']
         
-%s in %s
+    def icu(self):
+        return self.data.loc[self.location]['In intensive  care***']
 
-%s in %s
+#create the region object instances
+alberta = Region('In Alberta', data)
+calgary = Region('Calgary zone', data)
+edmonton = Region('Edmonton zone', data)
+central = Region('Central zone', data)
+north = Region('North zone', data)
+south = Region('South zone', data)
 
-%s in %s
-        
-%s in %s
-        
-%s in %s
+#Authenticate to Twitter using the API keys you obtained from your developer account - should be stored in environment variables
+auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"), 
+os.environ.get("CONSUMER_SECRET"))
+auth.set_access_token(os.environ.get("ACCESS_TOKEN"), 
+os.environ.get("ACCESS_TOKEN_SECRET"))
 
-%s in %s
+#create an authenticated object to interact with twitter's API
+covid_twt = tweepy.API(auth)
 
-#COVID19AB""" %(timeStatus,
-                    f"{int(albertaActive):,d}", alberta[3:10], 
-                    f"{int(calgaryActive):,d}", calgary, 
-                    f"{int(edmontonActive):,d}", edmonton, 
-                    f"{int(centralActive):,d}", central, 
-                    f"{int(southActive):,d}", south, 
-                    f"{int(northActive):,d}", north)))
-        caseUpdate
-    
-        
-        hospitalUpdate = covidAlberta.update_status(
-"""Current Hospitalizations:
-        
-%s in %s
 
-%s in %s
-
-%s in %s
-        
-%s in %s
-        
-%s in %s
-
-%s in %s
-
-#COVID19AB""" %(f"{int(albertaHospital):,d}", alberta[3:10], 
-                        f"{int(calgaryHospital):,d}", calgary, 
-                        f"{int(edmontonHospital):,d}", edmonton, 
-                        f"{int(centralHospital):,d}", central, 
-                        f"{int(southHospital):,d}", south, 
-                        f"{int(northHospital):,d}", north), in_reply_to_status_id=caseUpdate.id)
-    
-        hospitalUpdate
-
-        deathUpdate = covidAlberta.update_status(
-"""Total Deaths:
-        
-%s in %s
-
-%s in %s
-
-%s in %s
-        
-%s in %s
-        
-%s in %s
-
-%s in %s
-
-#COVID19AB""" %(f"{int(albertaDeaths):,d}", alberta[3:10], 
-                        f"{int(calgaryDeaths):,d}", calgary, 
-                        f"{int(edmontonDeaths):,d}", edmonton, 
-                        f"{int(centralDeaths):,d}", central, 
-                        f"{int(southDeaths):,d}", south, 
-                        f"{int(northDeaths):,d}", north), in_reply_to_status_id=hospitalUpdate.id)
-
-        deathUpdate
-
-        moreInfo = covidAlberta.update_status(
-"""For more detailed information, please visit:
-#COVID19AB
-https://www.alberta.ca/covid-19-alberta-data.aspx""", in_reply_to_status_id=deathUpdate.id)
-    
-        moreInfo
-
-    except tweepy.TweepError as error:
-        if error.api_code == 187:
-            #print a notice
-            print("Either no new updates or something else is wrong. Check code and Alberta Health website:")
-        else:
-            raise error
+#see if cases are duplicate
 
 
 
+#tweet cases
+cases = covid_twt.update_status('''%s - Update
+                         Active Cases:
 
-authenticateTwitter()
-tweet()
+Alberta: %s
+
+Calgary: %s
+
+Edmonton: %s
+
+Central AB: %s
+
+North AB: %s
+
+South AB: %s'''%(time, alberta.cases(), calgary.cases(), edmonton.cases(), central.cases(), north.cases(), south.cases()))
+
+#tweet hospitalizations
+hospitalizations = covid_twt.update_status(status='''%s - Update
+                         In Hospital:
+
+Alberta: %s
+
+Calgary: %s
+
+Edmonton: %s
+
+Central AB: %s
+
+North AB: %s
+
+South AB: %s'''%(time, alberta.hospital(), calgary.hospital(), edmonton.hospital(), central.hospital(), north.hospital(), south.hospital()), in_reply_to_status_id=cases.id)
+
+#tweet deaths
+deaths = covid_twt.update_status('''%s - Update
+                         In ICU:
+
+Alberta: %s
+
+Calgary: %s
+
+Edmonton: %s
+
+Central AB: %s
+
+North AB: %s
+
+South AB: %s'''%(time, alberta.icu(), calgary.icu(), edmonton.icu(), central.icu(), north.icu(), south.icu()), in_reply_to_status_id=hospitalizations.id)
+
+
+update_info = covid_twt.update_status(status=(latest_update + '\nPlease visit the Alberta Health website for more information\n' + url), in_reply_to_status_id=deaths.id)
+
+# %%
